c54a9a51709dbd7ccfa0937a2b9069e3e116db66
worm004 <worm004@hotmail.com>
2017-07-19 14:29:13 +0800

    comment on models to be downloaded

--- models/getModels.sh
+++ models/getModels.sh
@@ -1,7 +1,7 @@
 # "------------------------- POSE MODELS -------------------------"
 # Downloading body pose (COCO and MPI) as well as face models
 pose_folder="pose/"
-face_folder="face/"
+#face_folder="face/"
 
 # Body (COCO)
 coco_folder="$pose_folder"coco/""
@@ -11,14 +11,14 @@ if [ ! -f $coco_model ]; then
 fi
 
 # Body (MPI)
-mpi_folder="$pose_folder"mpi/""
-mpi_model="$mpi_folder"pose_iter_160000.caffemodel""
-if [ ! -f $mpi_model ]; then
-    wget http://posefs1.perception.cs.cmu.edu/OpenPose/models/$mpi_model -P $mpi_folder
-fi
-
-# Face
-face_model="$face_folder"pose_iter_116000.caffemodel""
-if [ ! -f $face_model ]; then
-    wget http://posefs1.perception.cs.cmu.edu/OpenPose/models/$face_model -P $face_folder
-fi
+#mpi_folder="$pose_folder"mpi/""
+#mpi_model="$mpi_folder"pose_iter_160000.caffemodel""
+#if [ ! -f $mpi_model ]; then
+#    wget http://posefs1.perception.cs.cmu.edu/OpenPose/models/$mpi_model -P $mpi_folder
+#fi
+#
+## Face
+#face_model="$face_folder"pose_iter_116000.caffemodel""
+#if [ ! -f $face_model ]; then
+#    wget http://posefs1.perception.cs.cmu.edu/OpenPose/models/$face_model -P $face_folder
+#fi
